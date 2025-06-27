cf1d01ee814890ddf98b74318dd5d590365173e2
Jarek <jarek.r@shodan9.com>
2025-06-27 16:19:26 +0200

    android r2

---
+++ android/project/obj/local/armeabi/objs/SDL2_mixer/dynamic_modplug.o.d
@@ -0,0 +1,2 @@
+/Volumes/BOOTCAMP/iProjects/GemsOfAtlantis/android/project/obj/local/armeabi/objs/SDL2_mixer/dynamic_modplug.o: \
+ /Volumes/BOOTCAMP/iProjects/GemsOfAtlantis/android/project/jni/SDL_mixer/dynamic_modplug.c
