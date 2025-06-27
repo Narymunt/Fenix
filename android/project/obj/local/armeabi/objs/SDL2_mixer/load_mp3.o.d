3b4b38d5a4d621baa46854f691f00c1c46650dfb
Jarek <jarek.r@shodan9.com>
2025-06-27 16:27:16 +0200

    r9

--- android/project/obj/local/armeabi/objs/SDL2_mixer/load_mp3.o.d
+++
@@ -1,2 +0,0 @@
-/Volumes/BOOTCAMP/iProjects/GemsOfAtlantis/android/project/obj/local/armeabi/objs/SDL2_mixer/load_mp3.o: \
- /Volumes/BOOTCAMP/iProjects/GemsOfAtlantis/android/project/jni/SDL_mixer/load_mp3.c
