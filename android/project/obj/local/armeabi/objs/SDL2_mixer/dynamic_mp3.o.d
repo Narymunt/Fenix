b342baeaff9101a0192f48c4471678d64878c00b
Jarek <jarek.r@shodan9.com>
2025-06-30 21:28:42 +0200

    r63

---
+++ android/project/obj/local/armeabi/objs/SDL2_mixer/dynamic_mp3.o.d
@@ -0,0 +1,2 @@
+/Volumes/BOOTCAMP/iProjects/GemsOfAtlantis/android/project/obj/local/armeabi/objs/SDL2_mixer/dynamic_mp3.o: \
+ /Volumes/BOOTCAMP/iProjects/GemsOfAtlantis/android/project/jni/SDL_mixer/dynamic_mp3.c
